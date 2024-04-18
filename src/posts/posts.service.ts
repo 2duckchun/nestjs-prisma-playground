fe4222fc9067fd99c985ad623a9da31af005a4be
2duckchun <kkts9308@gmail.com>
2024-04-18 10:04:07 +0900

    ✨ create post API 생성

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/database/prisma.service';
+import { CreatePostDto } from './dto';
 
 @Injectable()
-export class PostsService {}
+export class PostsService {
+  constructor(private prisma: PrismaService) {}
+
+  async create(createPostDto: CreatePostDto) {
+    return this.prisma.post.create({
+      data: {
+        title: createPostDto.title,
+        content: createPostDto.content,
+        authorId: createPostDto.authorId,
+      },
+    });
+  }
+
+  findAll() {
+    return this.prisma.post.findMany();
+  }
+}
